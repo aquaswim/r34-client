e0a2a848e47524a3b0a95128b4f766aa2922d7d4
aquaswim dev pc <bayusetiawan37@gmail.com>
2024-03-09 00:16:25 +0700

    fix: layout searchbar

--- ui/searchbar.go
+++ ui/searchbar.go
@@ -6,12 +6,41 @@ import (
 	"fyne.io/fyne/v2/widget"
 )
 
+type searchBarLayout struct {
+	padding float32
+}
+
+func (s searchBarLayout) Layout(objects []fyne.CanvasObject, containerSize fyne.Size) {
+	pos := fyne.NewPos(0, containerSize.Height-s.MinSize(objects).Height)
+	for _, o := range objects {
+		size := o.Size()
+		o.Resize(size)
+		o.Move(pos)
+
+		pos = pos.Add(fyne.NewPos(size.Width+s.padding, 0))
+	}
+}
+
+func (s searchBarLayout) MinSize(objects []fyne.CanvasObject) fyne.Size {
+	w, h := float32(0), float32(0)
+	for _, o := range objects {
+		childSize := o.Size()
+
+		w += childSize.Width + s.padding
+		h += childSize.Height
+	}
+	return fyne.NewSize(w, h)
+}
+
 func SearchBar(onSearch func(query string)) fyne.CanvasObject {
 	searchTextInput := widget.NewEntry()
 	searchButton := widget.NewButton("Search", func() {
 		onSearch(searchTextInput.Text)
 	})
 
-	c := container.NewVBox(searchTextInput, searchButton)
+	searchTextInput.Resize(fyne.NewSize(128, searchTextInput.MinSize().Height))
+	searchButton.Resize(searchButton.MinSize())
+
+	c := container.New(&searchBarLayout{padding: 5}, searchTextInput, searchButton)
 	return c
 }
